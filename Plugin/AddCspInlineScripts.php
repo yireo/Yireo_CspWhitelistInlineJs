bdda6946d2338318cc30f10d3d51e7fd48fc8ca9
Jisse Reitsma <jisse@yireo.com>
2024-08-02 15:29:00 +0200

    Add logger

--- Plugin/AddCspInlineScripts.php
+++ Plugin/AddCspInlineScripts.php
@@ -3,6 +3,7 @@
 namespace Yireo\CspWhitelistInlineJs\Plugin;
 
 use Magento\Framework\View\Element\Template;
+use Yireo\CspWhitelistInlineJs\Logger\ScriptLogger;
 use Yireo\CspWhitelistInlineJs\Util\ScriptFinder;
 use Yireo\CspWhitelistInlineJs\Util\SecureHtmlRendererStub;
 
@@ -10,18 +11,26 @@ class AddCspInlineScripts
 {
     private ScriptFinder $scriptFinder;
     private SecureHtmlRendererStub $secureHtmlRendererStub;
+    private ScriptLogger $scriptLogger;
 
     public function __construct(
         ScriptFinder $scriptFinder,
-        SecureHtmlRendererStub $secureHtmlRendererStub
+        SecureHtmlRendererStub $secureHtmlRendererStub,
+        ScriptLogger $scriptLogger
     ) {
         $this->scriptFinder = $scriptFinder;
         $this->secureHtmlRendererStub = $secureHtmlRendererStub;
+        $this->scriptLogger = $scriptLogger;
     }
 
     public function afterFetchView(Template $block, string $html): string
     {
         $scripts = $this->scriptFinder->find($html);
+        if (empty($scripts)) {
+            return $html;
+        }
+
+        $this->scriptLogger->log($block);
         foreach ($scripts as $fullScript => $inlineJs) {
             $newScript = $this->secureHtmlRendererStub->renderTag('script', [], $inlineJs, false);
             $html = str_replace($fullScript, $newScript, $html);
