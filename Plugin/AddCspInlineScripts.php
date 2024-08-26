0e494166fce23fd8c939771958432d848ac1f9cc
Jisse Reitsma <jisse@yireo.com>
2024-08-26 10:13:48 +0200

    Updates

--- Plugin/AddCspInlineScripts.php
+++ Plugin/AddCspInlineScripts.php
@@ -3,39 +3,26 @@
 namespace Yireo\CspWhitelistInlineJs\Plugin;
 
 use Magento\Framework\View\Element\Template;
+use Yireo\CspUtilities\Util\ReplaceInlineScripts;
 use Yireo\CspWhitelistInlineJs\Logger\ScriptLogger;
-use Yireo\CspWhitelistInlineJs\Util\ScriptFinder;
-use Yireo\CspWhitelistInlineJs\Util\SecureHtmlRendererStub;
 
 class AddCspInlineScripts
 {
-    private ScriptFinder $scriptFinder;
-    private SecureHtmlRendererStub $secureHtmlRendererStub;
+    private ReplaceInlineScripts $replaceInlineScripts;
     private ScriptLogger $scriptLogger;
 
     public function __construct(
-        ScriptFinder $scriptFinder,
-        SecureHtmlRendererStub $secureHtmlRendererStub,
+        ReplaceInlineScripts $replaceInlineScripts,
         ScriptLogger $scriptLogger
     ) {
-        $this->scriptFinder = $scriptFinder;
-        $this->secureHtmlRendererStub = $secureHtmlRendererStub;
+        $this->replaceInlineScripts = $replaceInlineScripts;
         $this->scriptLogger = $scriptLogger;
+
     }
 
-    public function afterFetchView(Template $block, string $html): string
+    public function afterToHtml(Template $block, $html): string
     {
-        $scripts = $this->scriptFinder->find($html);
-        if (empty($scripts)) {
-            return $html;
-        }
-
         $this->scriptLogger->log($block);
-        foreach ($scripts as $fullScript => $inlineJs) {
-            $newScript = $this->secureHtmlRendererStub->renderTag('script', [], $inlineJs, false);
-            $html = str_replace($fullScript, $newScript, $html);
-        }
-
-        return $html;
+        return $this->replaceInlineScripts->replace((string)$html);
     }
 }
